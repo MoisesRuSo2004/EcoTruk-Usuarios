c5423ab87be5e38378e6cb1888d51c178739371a
Moises Ruiz <moisesruizruizsoraca@gmail.com>
2025-11-24 22:53:09 -0500

    ajustes de api

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://ecotruck-dkfvh6e5brhqc6h5.brazilsouth-01.azurewebsites.net/api",
   timeout: 15000,
 });
 
